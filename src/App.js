fa314c37f34860dcb8546b841f16bd4e97a49b2c
phgiacomelli <101350035+phgiacomelli@users.noreply.github.com>
2022-06-24 09:54:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useTimer } from 'react-timer-hook';
+
+function MyTimer({ expiryTimestamp }) {
+  const {
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    resume,
+    restart,
+  } = useTimer({ expiryTimestamp, onExpire: () => console.warn('onExpire called') });
+
+
+  return (
+    <div style={{textAlign: 'center'}}>
+      <div style={{fontSize: '100px'}}>
+        {/* <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span> */}
+        <span>{minutes}</span>:<span>{seconds}</span>
+      </div>
+      <p>{isRunning ? 'Running' : 'Not running'}</p>
+      <button onClick={start}>Start</button>
+      <button onClick={pause}>Pause</button>
+      <button onClick={resume}>Resume</button>
+      <button onClick={() => {
+        // Restarts to 5 minutes timer
+        const time = new Date();
+        time.setSeconds(time.getSeconds() + 180);
+        restart(time)
+      }}>Restart</button>
+    </div>
+  );
+}
+
+function addTimer(){
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 300);
+  
+  const div = document.querySelector("#timers")
+  console.log(div)
+  const li = document.createElement("div")
+  li.innerHTML = `<MyTimer expiryTimestamp={time} />`
+  div.parentNode.insertBefore(li, div.nextSibling)
+  // div.appendChild(li)
+  
+}
 
 function App() {
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 300); // 10 minutes timer
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <div id='timers' >
+        
+          <button onClick={() => addTimer()}>
+            add timer
+          </button>
+        
+        {/* <li><MyTimer expiryTimestamp={time} /></li> */}
+      </div>
+      
     </div>
   );
 }
